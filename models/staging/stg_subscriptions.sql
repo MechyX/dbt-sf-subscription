5d20cc14e39ee00ebc740069f240afcf247c8fd7
mechy <52533782+MechyX@users.noreply.github.com>
2021-06-16 11:15:41 +0530

    Built Product Specific queries

--- models/staging/stg_subscriptions.sql
+++ models/staging/stg_subscriptions.sql
@@ -3,6 +3,7 @@ select sbqq__account__c as account,
        sbqq__productname__c as product_name,
        sbqq__productid__c as product_id,
        sbqq__netprice__c as net_price,
+       sbqq__quantity__c as quantity,
        case
         when sbqq__subscriptionstartdate__c__t is null then sbqq__startdate__c__t
         else sbqq__subscriptionstartdate__c__t
--- models/totals/total_mrr.sql
+++ models/totals/total_mrr.sql
@@ -2,7 +2,8 @@ with final as(
 SELECT month_range.year_month,
        sum(subscriptions.net_price) as MRR,
        count(distinct account) as customer_count,
-       sum(subscriptions.net_price) / count(distinct account) as ARPU
+       sum(subscriptions.net_price) / count(distinct account) as ARPU,
+       count(distinct product_id) as product_count
 from {{ref('stg_subscriptions')}} as subscriptions left join {{ref('dates')}} month_range on (month_range.year_month < subscriptions.end_date
 and month_range.year_month >= subscriptions.start_date)
 group by month_range.year_month)
